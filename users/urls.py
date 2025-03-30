8681d0fa10bccd1139ba0adaf9ffe1c11fcba7b4
Ann Lyapunova <lord.letopisniy@yandex.ru>
2025-03-30 16:31:39 +0300

    add homework 30_1

---
+++ users/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from users.apps import UsersConfig
+from users.views import (UserCreateApiView,
+                             UserDestroyApiView, UserListApiView,
+                             UserRetrieveApiView, UserUpdateApiView)
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path("", UserListApiView.as_view(), name="users_list"),
+    path("<int:pk>/", UserRetrieveApiView.as_view(), name="users_retrieve"),
+    path("create/", UserCreateApiView.as_view(), name="users_create"),
+    path(
+        "<int:pk>/delete/",
+        UserDestroyApiView.as_view(),
+        name="users_delete",
+    ),
+    path(
+        "<int:pk>/update/", UserUpdateApiView.as_view(), name="users_update"
+    ),
+]
